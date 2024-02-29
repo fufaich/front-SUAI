f5fbcca7c5ac7c6512911762af1f883cc8d1395a
fufaich <fufaich@gmail.com>
2024-03-01 00:09:21 +0300

    INITIAL commit

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="rounded-xl bg-gray-100 px-4 py-2">
+  <div class="rounded-xl bg-gray-100 px-4 py-2 hover:bg-gray-200 cursor-pointer  transition">
     <slot></slot>
   </div>
 </template>
